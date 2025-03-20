f4f29884fe33a24b3c691b3e46c17ed24d63872b
Skullian <133133667+Skullians@users.noreply.github.com>
2025-03-20 21:22:45 +0000

    Fix: Register BukkitAudiences [ci skip]
    ci skip until pr merge

--- src/main/java/net/skullian/util/ChatUtils.java
+++ src/main/java/net/skullian/util/ChatUtils.java
@@ -3,6 +3,7 @@ package net.skullian.util;
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.skullian.InteractiveChatPacketEvents;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
@@ -11,6 +12,10 @@ public class ChatUtils {
 
     public static BukkitAudiences audience;
     public static final String PREFIX = "<gray>[<reset><b><gradient:#0836FB:#00B6FF>InteractiveChat-PacketEvents</gradient></b><gray>]<reset>";
+
+    static {
+        audience = BukkitAudiences.create(InteractiveChatPacketEvents.instance);
+    }
 
     public static void sendMessage(Object message, CommandSender... senders) {
         if (senders.length == 0) senders = new ConsoleCommandSender[]{Bukkit.getConsoleSender()};
